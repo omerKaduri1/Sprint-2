7e653a161328df3d0ae4d1392c0bedcb5c8b02b2
omerKaduri1 <kaduriomer@gmail.com>
2024-02-28 11:13:43 +0200

    Initial basic structure on html+add necessary files

---
+++ js/services/util.service.js
@@ -0,0 +1,26 @@
+'use strict'
+function makeId(length = 5) {
+    var id = ''
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (var i = 0; i < length; i++) {
+        id += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return id
+}
+
+function getRandomColor() {
+	const letters = '0123456789ABCDEF'
+	let color = '#'
+
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)]
+	}
+	return color
+}
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min)
+    const maxFloored = Math.floor(max)
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
+}
