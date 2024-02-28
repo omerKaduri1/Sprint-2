47c5d3f72251341562b367f6e7e9e81060dc6677
omerKaduri1 <kaduriomer@gmail.com>
2024-02-28 12:08:32 +0200

    Implement renderMeme() and getMeme()

--- js/memeController.js
+++ js/memeController.js
@@ -1,1 +1,39 @@
 'use strict'
+
+var gElCanvas
+var gCtx
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    renderMeme()
+}
+
+function renderMeme() {
+    const meme = getMeme()
+    const img = new Image()
+    img.src = `img/${meme.selectedImgId}.jpg`
+    gElCanvas.height = (img.naturalHeight / img.naturalWidth) * gElCanvas.width
+    img.onload = () =>  {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+        drawText(meme.lines[0].txt, meme.lines[0].size, meme.lines[0].color)
+    }
+
+}
+
+function drawText(text = 'Hello Meme Generator!', size, color) {
+    const x = gElCanvas.width / 2
+    const y = gElCanvas.height / 2
+	gCtx.lineWidth = 2
+	gCtx.strokeStyle = color
+
+	// gCtx.fillStyle = 'whitesmoke'
+
+	gCtx.font = `${size}px Impact`
+	gCtx.textAlign = 'center'
+	// gCtx.textBaseline = 'middle'
+
+	gCtx.fillText(text, x, y)
+	gCtx.strokeText(text, x, y)
+}
--- js/services/memeService.js
+++ js/services/memeService.js
@@ -1,1 +1,18 @@
 'use strict'
+
+var gImgs = [{id: 1, url: 'img/1.jpg', keywords: ['funny', 'president']}]
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            color: 'red'
+        }
+    ]
+}
+
+function getMeme() {
+    return gMeme
+}
